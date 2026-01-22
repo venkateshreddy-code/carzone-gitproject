ffe48f5d008ed33bff74ddc54d6c9fdffa79ac6c
venky178 <venkateshreddyningam@gmail.com>
2026-01-22 07:16:13 -0500

    Frontend templates, static assets, and routing

--- carzone/settings.py
+++ carzone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,4 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+# settings.py
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # put ALL your css/js/img here during dev
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # only for collectstatic (production)
